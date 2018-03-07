ef8a17384b90a8458101c22982a494f0e1e42ac5
James Puellmann <james.puellmann@gmail.com>
2018-03-07 11:09:55 -0500

    update

--- .bashrc.d/exports.sh
+++ .bashrc.d/exports.sh
@@ -9,3 +9,5 @@ export VAGRANT_DEFAULT_PROVIDER=virtualbox
 export SPEC_OPTS="--format documentation"
 export rvm_path="${HOME}/.rvm"
 export PATH=${HOME}/anaconda3/bin:${PATH}
+
+export AWS_PROFILE=asynchrony-admin
